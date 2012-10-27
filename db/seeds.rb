dbe9dd4d4cdb06da6e09f51e6607bda6695df671
Jason Swett <jason@electricsasquatch.com>
2012-10-27 10:50:52 -0400

    Added state seed data. Also made seed passwords long enough to meet minimum length requirement.

--- db/seeds.rb
+++ db/seeds.rb
@@ -62,17 +62,75 @@ if Rails.env.development?
 
   # Make a default ADMIN user
   user = User.create(:email => "admin@example.com")
+<<<<<<< HEAD
   user.password = "adminadmin"
   user.password_confirmation = "adminadmin"
+=======
+  user.password = "adminpassword"
+  user.password_confirmation = "adminpassword"
+>>>>>>> Added state seed data. Also made seed passwords long enough to meet
   user.admin = true
   user.save!
 
   # Make a default MEMBER user
   user = User.create(:email => "member@example.com")
-  user.password = "member"
-  user.password_confirmation = "member"
+  user.password = "memberpassword"
+  user.password_confirmation = "memberpassword"
   user.admin = true
   user.save!
 
-
+  State.destroy_all
+  State.create([
+    { :name => 'Alabama', :abbreviation => 'AL'},
+    { :name => 'Alaska', :abbreviation => 'AK'},
+    { :name => 'Arizona', :abbreviation => 'AZ'},
+    { :name => 'Arkansas', :abbreviation => 'AR'},
+    { :name => 'California', :abbreviation => 'CA'},
+    { :name => 'Colorado', :abbreviation => 'CO'},
+    { :name => 'Connecticut', :abbreviation => 'CT'},
+    { :name => 'Delaware', :abbreviation => 'DE'},
+    { :name => 'District of Columbia', :abbreviation => 'DC'},
+    { :name => 'Florida', :abbreviation => 'FL'},
+    { :name => 'Georgia', :abbreviation => 'GA'},
+    { :name => 'Hawaii', :abbreviation => 'HI'},
+    { :name => 'Idaho', :abbreviation => 'ID'},
+    { :name => 'Illinois', :abbreviation => 'IL'},
+    { :name => 'Indiana', :abbreviation => 'IN'},
+    { :name => 'Iowa', :abbreviation => 'IA'},
+    { :name => 'Kansas', :abbreviation => 'KS'},
+    { :name => 'Kentucky', :abbreviation => 'KY'},
+    { :name => 'Louisiana', :abbreviation => 'LA'},
+    { :name => 'Maine', :abbreviation => 'ME'},
+    { :name => 'Maryland', :abbreviation => 'MD'},
+    { :name => 'Massachusetts', :abbreviation => 'MA'},
+    { :name => 'Michigan', :abbreviation => 'MI'},
+    { :name => 'Minnesota', :abbreviation => 'MN'},
+    { :name => 'Mississippi', :abbreviation => 'MS'},
+    { :name => 'Missouri', :abbreviation => 'MO'},
+    { :name => 'Montana', :abbreviation => 'MT'},
+    { :name => 'Nebraska', :abbreviation => 'NE'},
+    { :name => 'Nevada', :abbreviation => 'NV'},
+    { :name => 'New Hampshire', :abbreviation => 'NH'},
+    { :name => 'New Jersey', :abbreviation => 'NJ'},
+    { :name => 'New Mexico', :abbreviation => 'NM'},
+    { :name => 'New York', :abbreviation => 'NY'},
+    { :name => 'North Carolina', :abbreviation => 'NC'},
+    { :name => 'North Dakota', :abbreviation => 'ND'},
+    { :name => 'Ohio', :abbreviation => 'OH'},
+    { :name => 'Oklahoma', :abbreviation => 'OK'},
+    { :name => 'Oregon', :abbreviation => 'OR'},
+    { :name => 'Pennsylvania', :abbreviation => 'PA'},
+    { :name => 'Rhode Island', :abbreviation => 'RI'},
+    { :name => 'South Carolina', :abbreviation => 'SC'},
+    { :name => 'South Dakota', :abbreviation => 'SD'},
+    { :name => 'Tennessee', :abbreviation => 'TN'},
+    { :name => 'Texas', :abbreviation => 'TX'},
+    { :name => 'Utah', :abbreviation => 'UT'},
+    { :name => 'Vermont', :abbreviation => 'VT'},
+    { :name => 'Virginia', :abbreviation => 'VA'},
+    { :name => 'Washington', :abbreviation => 'WA'},
+    { :name => 'West Virginia', :abbreviation => 'WV'},
+    { :name => 'Wisconsin', :abbreviation => 'WI'},
+    { :name => 'Wyoming', :abbreviation => 'WY'}
+  ])
 end
