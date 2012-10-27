112ed2bb44b36f99e1277c1b8a601cb517bd06f4
Mark Van Holstyn <mvanholstyn@gmail.com>
2012-10-27 11:54:59 -0400

    allow editing name

--- app/models/item_field_definition.rb
+++ app/models/item_field_definition.rb
@@ -1,5 +1,5 @@
 class ItemFieldDefinition < ActiveRecord::Base
-  attr_accessible :label, :kind, :required, :unique, :item_type_definition_id
+  attr_accessible :name, :kind, :required, :unique, :item_type_definition_id
 
   belongs_to :item_type_definition
   
