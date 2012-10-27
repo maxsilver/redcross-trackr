736e3767df01fc6c94746263b7bbbd570f74fe6a
Mark Van Holstyn <mvanholstyn@gmail.com>
2012-10-27 11:54:24 -0400

    make item field def kind an enum to select from

--- app/models/item_field_definition.rb
+++ app/models/item_field_definition.rb
@@ -2,4 +2,8 @@ class ItemFieldDefinition < ActiveRecord::Base
   attr_accessible :label, :kind, :required, :unique, :item_type_definition_id
 
   belongs_to :item_type_definition
+  
+  def kind_enum
+    ["string", "number", "date"]
+  end
 end
