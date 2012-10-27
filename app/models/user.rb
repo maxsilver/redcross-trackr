baca8af0e1220d77f533a820f53f20fb4e47df9c
Jason Swett <jason@electricsasquatch.com>
2012-10-27 11:27:40 -0400

    Added Devise views.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :registerable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :admin
 end
