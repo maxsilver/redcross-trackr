b29d402aa65dd8779c07565585de118a11c4fb9c
Mark Van Holstyn <mvanholstyn@gmail.com>
2012-10-26 22:19:26 -0400

    Changed state and chapter fields to select fields.

--- features/step_definitions/location_steps.rb
+++ features/step_definitions/location_steps.rb
@@ -7,9 +7,16 @@ Given /^I am logged in as a member$/ do
 end
 
 When /^I create a location$/ do
-  visit new_location_path
   @location_name = "Red Cross Office"
+  FactoryGirl.create(:state, :name => "Michigan")
+  FactoryGirl.create(:chapter, :name => "Greater Grand Rapids")
+  visit new_location_path
   fill_in "Name", :with => @location_name
+  fill_in "Address", :with => "584 Penny Lane"
+  fill_in "City", :with => "Grand Rapids"
+  select "Michigan", :from => "State"
+  fill_in "Zip", :with => "49505"
+  select "Greater Grand Rapids", :from => "Chapter"
   click_on "Create Location"
 end
 
