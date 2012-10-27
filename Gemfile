93f039b01d3b48cbf8dbdb0ea70445aa6fba8f78
Mark Van Holstyn <mvanholstyn@gmail.com>
2012-10-27 01:48:03 -0400

    added admin role to keep non-admins out of rails admin

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem "paranoia"
 gem 'carrierwave'
 gem 'cloudinary'
 gem 'geocoder'
+gem 'cancan'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121027053311) do
+ActiveRecord::Schema.define(:version => 20121027054226) do
 
   create_table "chapters", :force => true do |t|
     t.string   "name"
@@ -20,37 +20,6 @@ ActiveRecord::Schema.define(:version => 20121027053311) do
     t.datetime "updated_at", :null => false
     t.integer  "region_id"
   end
-
-  create_table "geocodes", :force => true do |t|
-    t.decimal "latitude",    :precision => 15, :scale => 12
-    t.decimal "longitude",   :precision => 15, :scale => 12
-    t.string  "query"
-    t.string  "street"
-    t.string  "locality"
-    t.string  "region"
-    t.string  "postal_code"
-    t.string  "country"
-    t.string  "precision"
-  end
-
-  add_index "geocodes", ["country"], :name => "geocodes_country_index"
-  add_index "geocodes", ["latitude"], :name => "geocodes_latitude_index"
-  add_index "geocodes", ["locality"], :name => "geocodes_locality_index"
-  add_index "geocodes", ["longitude"], :name => "geocodes_longitude_index"
-  add_index "geocodes", ["postal_code"], :name => "geocodes_postal_code_index"
-  add_index "geocodes", ["precision"], :name => "geocodes_precision_index"
-  add_index "geocodes", ["query"], :name => "geocodes_query_index", :unique => true
-  add_index "geocodes", ["region"], :name => "geocodes_region_index"
-
-  create_table "geocodings", :force => true do |t|
-    t.integer "geocodable_id"
-    t.integer "geocode_id"
-    t.string  "geocodable_type"
-  end
-
-  add_index "geocodings", ["geocodable_id"], :name => "geocodings_geocodable_id_index"
-  add_index "geocodings", ["geocodable_type"], :name => "geocodings_geocodable_type_index"
-  add_index "geocodings", ["geocode_id"], :name => "geocodings_geocode_id_index"
 
   create_table "item_field_definitions", :force => true do |t|
     t.string   "label"
@@ -94,9 +63,8 @@ ActiveRecord::Schema.define(:version => 20121027053311) do
     t.string   "contact_phone"
     t.string   "national_shelter_system_identifier"
     t.integer  "chapter_id"
-    t.datetime "created_at",                                           :null => false
-    t.datetime "updated_at",                                           :null => false
-    t.boolean  "active",                             :default => true, :null => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
     t.datetime "deleted_at"
     t.string   "picture"
     t.float    "latitude"
@@ -131,8 +99,8 @@ ActiveRecord::Schema.define(:version => 20121027053311) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -141,8 +109,9 @@ ActiveRecord::Schema.define(:version => 20121027053311) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.boolean  "admin",                  :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
